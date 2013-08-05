6375c5dc31fd3d6d7ca909f12d2a462389580169
Kun Huang <academicgareth@gmail.com>
2013-08-05 11:15:48 +0800

    add file exist check in gen_img.sh

--- gen_img.sh
+++ gen_img.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 # Author: Kun Huang <academicgareth@gmail.com>
 
 mkdir -p images
 cd images
-wget http://mirrors.163.com/centos/6.4/os/x86_64/images/install.img
+pwd
+if [ ! -f install.img ];then
+    wget http://mirrors.163.com/centos/6.4/os/x86_64/images/install.img
+fi
 rm -rf test_iso
 mkdir -p test_iso
+if [ ! -f ../CentOS-6.4-x86_64-netinstall.iso ];then
+    wget http://mirrors.163.com/centos/6.4/isos/x86_64/CentOS-6.4-x86_64-netinstall.iso
+fi
 mount ../CentOS-6.4-x86_64-netinstall.iso test_iso
 rsync -P test_iso/isolinux/initrd.img .
 umount test_iso
