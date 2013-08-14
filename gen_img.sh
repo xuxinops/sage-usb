f8d0161d14c6d22d652442d420a8e23d31758bbf
Kun Huang <academicgareth@gmail.com>
2013-08-14 11:31:45 +0800

    improve fetching generate images script
    Change-Id: Idfa0a706e20393c207538204e0f3365556c040ab

--- gen_img.sh
+++ gen_img.sh
@@ -3,16 +3,10 @@
 
 mkdir -p images
 cd images
-pwd
 if [ ! -f install.img ];then
     wget http://mirrors.163.com/centos/6.4/os/x86_64/images/install.img
 fi
-rm -rf test_iso
-mkdir -p test_iso
-if [ ! -f ../CentOS-6.4-x86_64-netinstall.iso ];then
-    wget http://mirrors.163.com/centos/6.4/isos/x86_64/CentOS-6.4-x86_64-netinstall.iso
+if [ ! -f initrd.img ];then
+    wget http://mirrors.163.com/centos/6.4/os/x86_64/isolinux/initrd.img
 fi
-mount ../CentOS-6.4-x86_64-netinstall.iso test_iso
-rsync -P test_iso/isolinux/initrd.img .
-umount test_iso
-rm -rf test_iso
+md5sum -c md5.txt
