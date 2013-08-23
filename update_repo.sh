d985717f24f1702b48307c04021888c9741e27fb
Your full name <denny@unitedstack.com>
2013-08-23 18:47:54 +0800

    conditional check for localhost
    Change-Id: I5954104b510496f58fe07542ce83899c83280e92

--- update_repo.sh
+++ update_repo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
 # Author: Kun Huang <academicgareth@gmail.com>, DennyZhang <denny@unitedstack.com>
+# Sample: sudo ./update_repo.sh 127.0.0.1 /opt/repo
 
 server=${1:-192.168.1.5}
 repo_dir=${2:-/data1/mirrors/sunfire/RPMS/x86_64/}
 mkdir -p repo
 cd repo
-rsync -Pr root@$server:$repo_dir/* .
+if [ "$server"="127.0.0.1" ]; then
+    rsync -Pr $repo_dir/* .
+else
+    rsync -Pr root@$server:$repo_dir/* .
+fi;
 cd ..
